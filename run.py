f277453c4c4f083d80308c32bff5aafe9de0f843
LaMonaLisa34 <mona.vw@outlook.fr>
2025-08-16 10:52:05 +0200

    test

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     #app run -> demarre un serveur local sur http://127.0.0.1:5000/
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
